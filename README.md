951d4fd5e05168a6700701eac8ce566717052e7e
phuchieuct93abc <phuchieuct93@gmail.com>
2024-07-28 22:30:59 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-without_unsubscribe
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/phuchieuct93abc/RXJS-without_unsubscribe)
---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+import { of, delay } from 'rxjs';
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  template: `
+   app child
+  `,
+})
+export class AppChild implements OnInit {
+  ngOnInit() {
+    of('data')
+      .pipe(delay(5000))
+      .subscribe(() => console.log('DO some heavy task'));
+  }
+}
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `Toggle component 
+    <button (click)="render()">Render</button>
+   <div><app-child *ngIf="renderChild"></app-child></div>
+  `,
+  imports: [AppChild, CommonModule],
+})
+export class App {
+  renderChild = true;
+  render() {
+    this.renderChild = !this.renderChild;
+  }
+}
+
+bootstrapApplication(App);
